2e4dcdcc0f4ac286e4304e1e6584b297f8f1468a
Balázs Róbert Börcsök <pauljouser@protonmail.com>
2024-05-20 18:16:24 +0200

    Correctly showing coordinates.

--- MicroscopeTable/Components/VisualizationPanel.xaml.cs
+++ MicroscopeTable/Components/VisualizationPanel.xaml.cs
@@ -1,20 +1,55 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
-
-using MicroscopeTable.Components;
 
 namespace MicroscopeTable.Components
 {
     public partial class VisualizationPanel : UserControl
     {
-        private Table table;
+        private Point center;
+
         public VisualizationPanel()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+            SizeChanged += OnSizeChanged;
+            MainCanvas.MouseMove += OnMouseMove;
+        }
 
-            table = new Table();
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            UpdateCenter();
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateCenter();
+        }
+
+        private void UpdateCenter()
+        {
+            center = new Point(MainCanvas.ActualWidth / 2, MainCanvas.ActualHeight / 2);
+            UpdateCenterLines();
+        }
+
+        private void UpdateCenterLines()
+        {
+            HorizontalLine.X1 = 0;
+            HorizontalLine.Y1 = center.Y;
+            HorizontalLine.X2 = MainCanvas.ActualWidth;
+            HorizontalLine.Y2 = center.Y;
+
+            VerticalLine.X1 = center.X;
+            VerticalLine.Y1 = 0;
+            VerticalLine.X2 = center.X;
+            VerticalLine.Y2 = MainCanvas.ActualHeight;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(MainCanvas);
+            var relativePosition = new Point(position.X - center.X, center.Y - position.Y);
+            CoordinateTextBlock.Text = $"X: {relativePosition.X:F2}, Y: {relativePosition.Y:F2}";
         }
     }
 }
