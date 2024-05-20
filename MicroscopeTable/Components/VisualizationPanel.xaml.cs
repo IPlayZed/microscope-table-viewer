cb5bbb11a74c68c6b1f6c12dbdbcaf131c53431d
Balázs Róbert Börcsök <pauljouser@protonmail.com>
2024-05-20 21:29:33 +0200

    Added animation + renamed right side panel.

--- MicroscopeTable/Components/VisualizationPanel.xaml.cs
+++ MicroscopeTable/Components/VisualizationPanel.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace MicroscopeTable.Components
@@ -9,6 +10,8 @@ namespace MicroscopeTable.Components
     {
         private Point center;
         private double zCoordinate;
+        private double zoomFactor = 1.0;
+        private const double zoomIncrement = 0.1;
 
         public VisualizationPanel()
         {
@@ -22,11 +25,13 @@ namespace MicroscopeTable.Components
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
             UpdateCenter();
+            PositionMicroscopeTable();
         }
 
         private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
             UpdateCenter();
+            PositionMicroscopeTable();
         }
 
         private void UpdateCenter()
@@ -57,7 +62,16 @@ namespace MicroscopeTable.Components
 
         private void OnMouseWheel(object sender, MouseWheelEventArgs e)
         {
-            zCoordinate += e.Delta > 0 ? 1 : -1;
+            zCoordinate += e.Delta > 0 ? 1 : -1; // Update Z coordinate on scroll
+
+            zoomFactor += e.Delta > 0 ? zoomIncrement : -zoomIncrement;
+            zoomFactor = Math.Max(0.1, zoomFactor); // Prevent zooming out too much
+
+            MicroscopeTableRect.Width = 200 * zoomFactor;
+            MicroscopeTableRect.Height = 150 * zoomFactor;
+
+            PositionMicroscopeTable();
+
             var position = Mouse.GetPosition(MainCanvas);
             var relativePosition = new Point(position.X - center.X, center.Y - position.Y);
             UpdateCoordinateText(relativePosition);
@@ -66,6 +80,30 @@ namespace MicroscopeTable.Components
         private void UpdateCoordinateText(Point relativePosition)
         {
             CoordinateTextBlock.Text = $"X: {relativePosition.X:F2}, Y: {relativePosition.Y:F2}, Z: {zCoordinate:F2}";
+        }
+
+        private void PositionMicroscopeTable()
+        {
+            Canvas.SetLeft(MicroscopeTableRect, center.X - MicroscopeTableRect.Width / 2);
+            Canvas.SetTop(MicroscopeTableRect, center.Y - MicroscopeTableRect.Height / 2);
+        }
+
+        private void MainCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var targetPosition = e.GetPosition(MainCanvas);
+            AnimateMicroscopeTable(targetPosition);
+        }
+
+        private void AnimateMicroscopeTable(Point targetPosition)
+        {
+            double targetLeft = targetPosition.X - MicroscopeTableRect.Width / 2;
+            double targetTop = targetPosition.Y - MicroscopeTableRect.Height / 2;
+
+            DoubleAnimation animX = new DoubleAnimation(Canvas.GetLeft(MicroscopeTableRect), targetLeft, TimeSpan.FromSeconds(0.5));
+            DoubleAnimation animY = new DoubleAnimation(Canvas.GetTop(MicroscopeTableRect), targetTop, TimeSpan.FromSeconds(0.5));
+
+            MicroscopeTableRect.BeginAnimation(Canvas.LeftProperty, animX);
+            MicroscopeTableRect.BeginAnimation(Canvas.TopProperty, animY);
         }
     }
 }
